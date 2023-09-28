94e12948c367e170711f0749a64bdce844efd28b
CristobalMoya7 <CristobalMoyaLorente00@gmail.com>
2023-09-28 19:30:37 +0200

    Actualizacion 3

---
+++ list_utils.py
@@ -0,0 +1,6 @@
+def find_one(list, needle):
+    """
+    Devuelve TRUE si encuentra una o mas ocurrencia de needle en list
+    """
+    # Mientras no encontramos o hayamos terminado con la lista
+    # Miramos a ver si esta en la posicion actual y actualizamos la condición
